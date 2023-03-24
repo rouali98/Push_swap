84c343cfe3c0de1080ba19febb607dc609c58547
Rida Ouali <rouali@c1r1p1.1337.ma>
2023-03-24 15:53:43 +0000

    update push-swap

--- sort_hundred.c
+++ sort_hundred.c
@@ -3,67 +3,91 @@
 /*                                                        :::      ::::::::   */
 /*   sort_hundred.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rouali <rouali@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: rouali <rouali@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 10:22:31 by rouali            #+#    #+#             */
-/*   Updated: 2023/03/21 19:39:54 by rouali           ###   ########.fr       */
+/*   Updated: 2023/03/24 15:50:07 by rouali           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	ft_min_hundred(void)
-{
-	int	i;
-	int	min;
+//int	ft_min_hundred(void)
+//{
+//	int	i;
+//	int	min;
 
-	i = 0;
-	di.x = 0;
-	min = stack_s.stacka[0];
-	while (di.x < ps.contare)
-	{
-		if (stack_s.stacka[di.x] < min)
-		{
-			min = stack_s.stacka[di.x];
-		}
-		di.x++;
-	}
-	i++;
-	return (min);
-}
+//	i = 0;
+//	di.x = 0;
+//	min = stack_s.stacka[0];
+//	while (di.x < ps.contare)
+//	{
+//		if (stack_s.stacka[di.x] < min)
+//		{
+//			min = stack_s.stacka[di.x];
+//		}
+//		di.x++;
+//	}
+//	i++;
+//	return (min);
+//}
 
-void	ft_pa_rra_hundred(void)
-{
-	int	min;
-	int	ph;
+//void	ft_pa_rra_hundred(void)
+//{
+//	int	min;
+//	int	ph;
 
-	ph = 0;
-	min = ft_min();
-	while (ph < ps.contare)
-	{
-		if (stack_s.stacka[0] == min)
-		{
-			push_a();
-			ps.contare = ps.contare - 1;
-			break ;
-		}
-		else
-		{
-			ft_rev_a_shift_down();
-			f_print("rra");
-		}
-		ph++;
-	}
-}
+//	ph = 0;
+//	min = ft_min();
+//	while (ph < ps.contare)
+//	{
+//		if (stack_s.stacka[0] == min)
+//		{
+//			push_a();
+//			ps.contare = ps.contare - 1;
+//			break ;
+//		}
+//		else
+//		{
+//			ft_rev_a_shift_down();
+//			f_print("rra");
+//		}
+//		ph++;
+//	}
+//}
+
+//void	ft_sort_hundred(void)
+//{
+//	int	i = 0;
+
+//	while (i <= 5)
+//	{
+//		ft_pa_rra();
+//		i++;
+//	}
+//	ps.contare = ps.contare + 1;
+//}
 
 void	ft_sort_hundred(void)
 {
-	int	i = 0;
+	int	x;
+	int	y;
 
-	while (i <= 5)
+	x = 0;
+	while (stack_s.stacka[x])
 	{
-		ft_pa_rra();
-		i++;
+		y = 10;
+		if (stack_s.stacka[x] >= x && stack_s.stacka[x] <= y)
+		{
+			push_a();
+			ps.contare = ps.contare - 1;
+		}
+		else
+		{
+			x++;
+			y++;
+		}
+		x++;
+		y++;
 	}
-	ps.contare = ps.contare + 1;
 }
